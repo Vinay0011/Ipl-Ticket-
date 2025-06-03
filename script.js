5efc532e5e5c3243f2504cbeb25f2a865551f016
Vinay0011 <132700694+Vinay0011@users.noreply.github.com>
2025-06-03 11:13:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function showDetails(id) {
-  const paras = document.querySelectorAll(".category p");
-  paras.forEach(p => p.classList.add("hidden"));
 
-  const para = document.getElementById(id);
-  if (para) {
-    para.classList.remove("hidden");
-  }
-}
